f4abf187dccfc8b2f7e66903721e8b404e2f60fd
fralps <francois@kinoba.fr>
2025-10-31 15:37:06 +0100

    feat: update dashboard stats controller specs to include detailed sleep type counts

--- spec/request/api/v1/stats/dashboard_stats_controller_spec.rb
+++ spec/request/api/v1/stats/dashboard_stats_controller_spec.rb
@@ -1,82 +1,109 @@
 # frozen_string_literal: true
 
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.describe Api::V1::Stats::DashboardStatsController, type: :request do
-#   context 'when user has records' do
-#     describe '#GET /api/v1/stats/dashboard_stats' do
-#       let(:user) { create(:user) }
-#       let(:sleeps_count) { 10 }
+RSpec.describe Api::V1::Stats::DashboardStatsController, type: :request do
+  context 'when user has records' do
+    describe '#GET /api/v1/stats/dashboard_stats' do
+      let(:user) { create(:user) }
 
-#       before do
-#         create_list(:sleep, sleeps_count, user:)
-#         get '/api/v1/stats/dashboard_stats', headers: auth_headers(user)
-#       end
+      before do
+        create_list(:sleep, 10, sleep_type: 'dream', user: user)
+        create_list(:sleep, 5, sleep_type: 'lucid', user: user)
+        create_list(:sleep, 2, sleep_type: 'nightmare', user: user)
+        create_list(:sleep, 8, sleep_type: 'sleep_paralysis', user: user)
+        create_list(:sleep, 4, sleep_type: 'sleep_walking', user: user)
+        create_list(:sleep, 3, sleep_type: 'sleep_talking', user: user)
+        create_list(:sleep, 6, sleep_type: 'sleep_apnea', user: user)
+        create_list(:sleep, 1, sleep_type: 'erotic', user: user)
+        get '/api/v1/stats/dashboard_stats', headers: auth_headers(user)
+      end
 
-#       it_behaves_like 'A success response'
+      it_behaves_like 'A success response'
 
-#       it 'returns the current_user totals json key' do
-#         expect(json_response).to include('totals')
-#       end
+      it 'returns the current_user totals json key' do
+        expect(json_response).to include('totals')
+      end
 
-#       it 'returns the current_user by_month json key' do
-#         expect(json_response).to include('by_month')
-#       end
+      # it 'returns the current_user by_month json key' do
+      #   expect(json_response).to include('by_month')
+      # end
 
-#       it 'returns the current_user by_year json key' do
-#         expect(json_response).to include('by_year')
-#       end
+      # it 'returns the current_user by_year json key' do
+      #   expect(json_response).to include('by_year')
+      # end
 
-#       # TOTALS
-#       it 'returns correct dreams total count' do
-#         expect(json_response['totals']['dreams']).to eq(dreams_count)
-#       end
+      # TOTALS
+      it 'returns correct dreams total count' do
+        expect(json_response['totals']['dream']).to eq(10)
+      end
 
-#       it 'returns correct lucids total count' do
-#         expect(json_response['totals']['lucids']).to eq(lucids_count)
-#       end
+      it 'returns correct lucids total count' do
+        expect(json_response['totals']['lucid']).to eq(5)
+      end
 
-#       it 'returns correct nightmares total count' do
-#         expect(json_response['totals']['nightmares']).to eq(nightmares_count)
-#       end
+      it 'returns correct nightmares total count' do
+        expect(json_response['totals']['nightmare']).to eq(2)
+      end
 
-#       # BY_MONTH
-#       it 'returns dreams count by month' do
-#         expect(json_response['by_month']['dreams']).not_to be_nil
-#       end
+      it 'returns correct sleep_paralysis total count' do
+        expect(json_response['totals']['sleep_paralysis']).to eq(8)
+      end
 
-#       it 'returns lucids count by month' do
-#         expect(json_response['by_month']['lucids']).not_to be_nil
-#       end
+      it 'returns correct sleep_walking total count' do
+        expect(json_response['totals']['sleep_walking']).to eq(4)
+      end
 
-#       it 'returns nightmares count by month' do
-#         expect(json_response['by_month']['nightmares']).not_to be_nil
-#       end
+      it 'returns correct sleep_talking total count' do
+        expect(json_response['totals']['sleep_talking']).to eq(3)
+      end
 
-#       # BY_YEAR
-#       it 'returns dreams count by year' do
-#         expect(json_response['by_year']['dreams']).not_to be_nil
-#       end
+      it 'returns correct sleep_apnea total count' do
+        expect(json_response['totals']['sleep_apnea']).to eq(6)
+      end
 
-#       it 'returns lucids count by year' do
-#         expect(json_response['by_year']['lucids']).not_to be_nil
-#       end
+      it 'returns correct erotic total count' do
+        expect(json_response['totals']['erotic']).to eq(1)
+      end
+    end
 
-#       it 'returns nightmares count by year' do
-#         expect(json_response['by_year']['nightmares']).not_to be_nil
-#       end
-#     end
-#   end
+    #     # BY_MONTH
+    #     it 'returns dreams count by month' do
+    #       expect(json_response['by_month']['dreams']).not_to be_nil
+    #     end
 
-#   context 'when user does not have records' do
-#     describe '#GET /api/v1/stats/dashboard_stats' do
-#       let(:user) { create(:user) }
+    #     it 'returns lucids count by month' do
+    #       expect(json_response['by_month']['lucids']).not_to be_nil
+    #     end
 
-#       before do
-#         get '/api/v1/stats/dashboard_stats', headers: auth_headers(user)
-#       end
+    #     it 'returns nightmares count by month' do
+    #       expect(json_response['by_month']['nightmares']).not_to be_nil
+    #     end
 
-#       it_behaves_like 'A success response'
-#     end
-#   end
-# end
+    #     # BY_YEAR
+    #     it 'returns dreams count by year' do
+    #       expect(json_response['by_year']['dreams']).not_to be_nil
+    #     end
+
+    #     it 'returns lucids count by year' do
+    #       expect(json_response['by_year']['lucids']).not_to be_nil
+    #     end
+
+    #     it 'returns nightmares count by year' do
+    #       expect(json_response['by_year']['nightmares']).not_to be_nil
+    #     end
+    #   end
+  end
+
+  context 'when user does not have records' do
+    describe '#GET /api/v1/stats/dashboard_stats' do
+      let(:user) { create(:user) }
+
+      before do
+        get '/api/v1/stats/dashboard_stats', headers: auth_headers(user)
+      end
+
+      it_behaves_like 'A success response'
+    end
+  end
+end
